cee9ac0efb77a44f5fe3e6710c72e8eadb7dde2c
Kacper Śniadek <k.cs.kesz@gmail.com>
2023-11-09 23:59:35 +0100

    last fresh start 2

--- src/main/java/pl/kacperSniadek/BookshopApplication/controller/HomeController.java
+++ src/main/java/pl/kacperSniadek/BookshopApplication/controller/HomeController.java
@@ -1,12 +1,23 @@
 package pl.kacperSniadek.BookshopApplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import pl.kacperSniadek.BookshopApplication.model.Book;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Controller
 public class HomeController {
+    private List<Book> books = List.of(
+            new Book("name1", "author1", new BigDecimal("199.99")),
+            new Book("name2", "author2", new BigDecimal("299.99")),
+            new Book("name3", "author3", new BigDecimal("399.99"))
+    );
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("books", books);
         return "home";
     }
 }
---
+++ src/main/java/pl/kacperSniadek/BookshopApplication/model/Book.java
@@ -0,0 +1,22 @@
+package pl.kacperSniadek.BookshopApplication.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@NoArgsConstructor
+@Getter
+@Setter
+public class Book {
+    private String name;
+    private String author;
+    private BigDecimal price;
+
+    public Book(String name, String author, BigDecimal price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+}
