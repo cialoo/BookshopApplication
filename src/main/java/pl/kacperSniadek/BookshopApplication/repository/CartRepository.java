5a3c3b98c548d3f0586d68c75e7d7413a2a6b590
Kacper Śniadek <k.cs.kesz@gmail.com>
2023-11-09 23:39:33 +0100

    last fresh start 1

--- src/main/java/pl/kacperSniadek/BookshopApplication/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package pl.kacperSniadek.BookshopApplication.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pl.kacperSniadek.BookshopApplication.models.Cart;
-
-@Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
-}
